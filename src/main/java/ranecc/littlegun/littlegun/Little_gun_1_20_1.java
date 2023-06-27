4d403ded562e9f387dfada98969b25cb7653d280
rsgzl-true <237788936@qq.com>
2023-06-27 11:20:48 +0800

    little-gun

---
+++ src/main/java/ranecc/littlegun/littlegun/Little_gun_1_20_1.java
@@ -0,0 +1,13 @@
+package ranecc.littlegun.littlegun;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Little_gun_1_20_1 implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/ranecc/littlegun/littlegun/client/Little_gun_1_20_1Client.java
@@ -0,0 +1,13 @@
+package ranecc.littlegun.littlegun.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Little_gun_1_20_1Client implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
